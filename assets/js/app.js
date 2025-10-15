cf6e743ba5458c33b8f5675bdb36297a41fbd30c
Juan Mahecha <mahechajuanda29@gmail.com>
2025-10-15 17:31:04 -0500

    agregue la parte del modal en el dashboard y le estoy terminando de hacer la funcion en app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             peliculas = data;
 
-            // mostrarPeliculas();
-            // Aqui iran las futuras funciones a realizar 
+            mostrarPeliculas(); 
         })
     
 
@@ -23,6 +22,50 @@ document.addEventListener('DOMContentLoaded', () => {
         })
 });
 
+function mostrarPeliculas(){
+    contenedor.innerHTML = "";
+
+    peliculas.forEach(pelicula =>{
+        const item =document.createElement('article');
+        item.classList.add('movie-card');
+
+        item.innerHTML = `
+            <img src="assets/${pelicula.ruta_caratula}" alt="${pelicula.nombre}" class="movie-poster">
+            <div class="movie-meta">
+                <h2 class="movie-title">${pelicula.nombre}</h2>
+                <span class="movie-year">${pelicula.anio}</span>
+            </div>
+        `;
+
+        item.addEventListener('click', () => mostrarDetalle(pelicula));
+        contenedor.appendChild(item);
+
+    })
+}
+function mostrarDetalle(pelicula) {
+    const detalle = document.createElement('section');
+    detalle.classList.add('detalle-pelicula');
+
+    detalle.innerHTML = `
+        <div class="detalle-content">
+            <button class="cerrar-detalle">✖</button>
+            <img src="assets/${pelicula.ruta_caratula}" alt="${pelicula.nombre}" class="detalle-poster">
+            <div class="detalle-info">
+                <h2>${pelicula.nombre}</h2>
+                <p><strong>Año:</strong> ${pelicula.anio}</p>
+                <p><strong>Género:</strong> ${pelicula.genero}</p>
+                <p><strong>Descripción:</strong> ${pelicula.descripcion}</p>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(detalle);
+
+    detalle.querySelector('.cerrar-detalle').addEventListener('click', () =>{
+        detalle.remove();
+    });
+
+}
 
 fetch("/assets/data/peliculas.json")
 
